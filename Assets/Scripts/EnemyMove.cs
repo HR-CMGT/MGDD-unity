9fb6839f440beb6237af55c93f5f41ceeb36d67d
Kokodoko <benni_jolink@hotmail.com>
2017-08-29 12:19:41 +0200

    enemy script

--- Assets/Scripts/EnemyMove.cs
+++ Assets/Scripts/EnemyMove.cs
@@ -1,14 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Move : MonoBehaviour {
+public class EnemyMove : MonoBehaviour {
 
 	// verander private naar public om in de editor te kunnen tweaken met de waarden
 	private Vector2 Speed = new Vector2(-1f,0);
 
-	void Update() {
-		// movement voor non-physics objects
-		transform.Translate(Speed * Time.deltaTime);
+	void Start () {
+		// movement voor physics objects
+	    GetComponent<Rigidbody2D> ().velocity = Speed;
 	}
 		
 }
