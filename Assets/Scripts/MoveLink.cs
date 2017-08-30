6d023e25555b4b90893f7d2e97dfa3f92c1ee552
Kokodoko <benni_jolink@hotmail.com>
2017-08-30 22:50:46 +0200

    Added Link

---
+++ Assets/Scripts/MoveLink.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveLink : MonoBehaviour {
+
+	public float moveSpeed = 3f;
+
+	void Update() {
+		// move link
+		Vector3 dir = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+		transform.Translate(dir.normalized * moveSpeed * Time.deltaTime);
+
+		// pass horizontal movement to the animator
+		GetComponent<Animator>().SetFloat("speed", dir.x);
+	}
+}
