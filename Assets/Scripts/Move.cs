9f8114b71dbdd50ddae76fc22d7fae8f6a0c72c7
Erik Katerborg <benni_jolink@hotmail.com>
2019-08-29 15:48:50 +0200

    Update Move.cs

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -1,14 +1,15 @@
-﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-public class Move : MonoBehaviour {
+public class Move : MonoBehaviour
+{
+    // verander private naar public om in de editor te kunnen tweaken met de waarden
+    private Vector2 Speed = new Vector2(-1f, 0);
 
-	// verander private naar public om in de editor te kunnen tweaken met de waarden
-	private Vector2 Speed = new Vector2(-1f,0);
-
-	void Update() {
-		// movement voor non-physics objects
-		transform.Translate(Speed * Time.deltaTime);
-	}
-		
+    void Update()
+    {
+        // movement voor non-physics objects
+        transform.Translate(Speed * Time.deltaTime);
+    }
 }
